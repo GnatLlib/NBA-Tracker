2e9adeb6240cb2d19500eb63be720ab06103d9c0
Bill Tang <billt4ng@gmail.com>
2016-11-03 20:00:16 -0700

    waitress

--- NBATracker/settings.py
+++ NBATracker/settings.py
@@ -124,13 +124,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_PATH = os.path.join(PROJECT_PATH,'static')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_URL = '/static/' # You may find this is already defined as such.
-
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    STATIC_PATH,
+    os.path.join(PROJECT_ROOT, 'static'),
 )
+
 
 
