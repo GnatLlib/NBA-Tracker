79639b85cb49a4f51c3359c970c8177221e7d674
Bill Tang <billt4ng@gmail.com>
2016-11-05 16:11:12 -0700

    fixing postgres

--- NBATracker/settings.py
+++ NBATracker/settings.py
@@ -93,8 +93,7 @@ DATABASES = {
 }
 
 import dj_database_url
-
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
